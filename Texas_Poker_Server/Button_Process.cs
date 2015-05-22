9af6ebafd5f78caa24fd98fe10b87287ae73bfd1
TzuWei-LIn <freelifetw29@gmail.com>
2015-05-23 02:55:18 +0800

    Version 3.1

---
+++ Texas_Poker_Server/Button_Process.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texas_Poker_Server
+{
+    class Button_Process : Game_Round
+    {
+        public String Event_Process(String a,int Raise_money,int location)
+        {
+            String[] b = a.Split(' ');
+            switch (b[0])
+            {
+                case "Call":
+                    if (int.Parse(b[1]) > Raise_money)
+                        Raise_money = int.Parse(b[1]);
+                    Total_money += int.Parse(b[1]);
+                    Console.WriteLine("GG = {0}",call_ppl);
+                    call_ppl++;
+
+                    Player_money[location] = int.Parse(b[2]);
+                    return Raise_money.ToString() + " call";
+                case "Raise":
+                    Raise_money = int.Parse(b[1]);
+                    Total_money += int.Parse(b[2]);
+                    //Player_money[location] = Player_money[location] - int.Parse(b[2]);
+                    Player_money[location] = int.Parse(b[3]);
+                    call_ppl = 0;
+                    return Raise_money.ToString() + " rasie";
+                case "Fold":
+                    //Player_state.Remove(location);
+                    return Raise_money.ToString() + " fold";
+            }
+            return "GG";
+        }
+
+    }
+}
---
+++ Texas_Poker_Server/Winner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texas_Poker_Server
+{
+    class Winner : Game_Round
+    {
+        public Winner(int location)
+        {
+            Player_money[location] += Total_money;
+        }
+    }
+}
